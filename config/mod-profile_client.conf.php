9d4e98b38cc4586d99b5440dfe194133c6cefd51
Payam Naderi <naderi.payam@gmail.com>
2018-05-11 18:47:06 +0430

    code cleanup

--- config/mod-profile_client.conf.php
+++ config/mod-profile_client.conf.php
@@ -5,7 +5,7 @@ use Module\ProfileClient\Services\ServiceClientOfProfile;
 return [
     Module::CONF => [
         ServiceClientOfProfile::CONF => [
-            'server_url'     => 'http://0.0.0.0',
+            'server_url'     => 'http://127.0.0.1',
 
             #'token_provider' => '/module/clientProfile/services/TokenProvider',
             'token_provider' => new \Poirot\Ioc\instance(
