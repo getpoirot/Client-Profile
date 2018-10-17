aa9f3f59dee3cb107dd15d24f369382f52a9b982
Payam Naderi <naderi.payam@gmail.com>
2018-10-17 11:58:06 +0330

    fix parsing unexpected data response

--- src/Client/Response.php
+++ src/Client/Response.php
@@ -4,6 +4,7 @@ namespace Poirot\ProfileClient\Client;
 use Poirot\ApiClient\Exceptions\exServerError;
 use Poirot\ApiClient\Exceptions\exUnknownError;
 use Poirot\ApiClient\HttpResponseOfClient;
+use Poirot\Std\Interfaces\Struct\iDataEntity;
 
 
 class Response
@@ -13,9 +14,18 @@ class Response
      * // TODO handle token issues
      *
      * @param \Poirot\Std\Struct\DataEntity $expected
+     *
+     * @throws \Exception
      */
     function doRecognizeErrorFromExpected($expected)
     {
+        if (! $expected instanceof iDataEntity)
+            throw new \Exception(sprintf(
+                'Bad Data Response; get: (%s).'
+                , \Poirot\Std\flatten($expected)
+            ));
+
+
         $error = $expected->get('error');
         if (! $error )
             // Unknown Error Happen
